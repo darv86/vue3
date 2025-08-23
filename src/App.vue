12b934752aa5e1333743aaa7beb5c7f5f5f1b76d
Darv <86darv@gmail.com>
2025-08-23 20:23:38 +0300

    components, slots, named slots, dynamic name for slots

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
-const color = 'blue';
+import Button from './components/Button.vue';
+
+const iconText = 'iconText';
 </script>
 
 <template>
 	<div class="board">
-		<div :class="`${color}`">sup</div>
-		<div v-bind:class="`${color}`">hi</div>
+		<Button>
+			<!-- data for the named slot should be placed inside the template tag
+			with a v-slot attribute and slot name -->
+			<!-- <template v-slot:icon>+</template> -->
+			<template #icon>+</template>
+			<!-- dynamic name for a slot (slot name interpolation) -->
+			<template #[iconText]>Add </template>
+			<span class="btn__span">Button</span>
+		</Button>
 	</div>
 </template>
 
---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+	<button class="btn">
+		<!-- named slot -->
+		<slot name="icon"></slot>
+		<slot name="iconText"></slot>
+		<!-- slot receives any tags and text (span with text) that Button components wraps -->
+		<slot></slot>
+	</button>
+</template>
+
+<!-- attribute 'scoped' adds to the html tag additional data-attribute with component id
+and applies styles for these components only -->
+<style scoped>
+.btn {
+	font-size: 4rem;
+}
+</style>
